2de49e282a456a4a8fa048a1cb1642cd90073efe
peter_mihalik <pitu.akirko@gmail.com>
2022-04-05 14:06:10 +0200

    TaskList.js version 1.0

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,12 @@
 import './styles/taskList.css'
+import './Task'
 
-export default function TaskList() {
+export default function TaskList({tasks}) {
   return (
-    <div>TaskList</div>
+    <div className='taskList'>
+        {tasks.map((task) => (
+            <Task task={task}></Task>
+        ))}
+    </div>
   )
 }
